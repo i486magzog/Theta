06976af45e4e5c863ff27b685745e9ad138cc3f4
development@magzog.com <development@magzog.com>
2025-04-28 21:40:59 +1200

    Except pages/packages from gitignore.

---
+++ theta.client/src/app/pages/packages/packages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-packages',
+  standalone: false,
+  templateUrl: './packages.component.html',
+  styleUrl: './packages.component.css'
+})
+export class PackagesComponent {
+
+}
